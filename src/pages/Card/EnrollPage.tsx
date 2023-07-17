c35ae553eaf8539a4b5ec00de0ed737abd02824c
ajaj0123 <jenny3976255@gmail.com>
2023-07-17 21:28:11 +0900

    style: 코드 formatting (css -> tailwind 변경)

--- src/pages/Card/EnrollPage.tsx
+++ src/pages/Card/EnrollPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../../css/enroll.css";
 
 function EnrollPage() {
   const [name, setName] = useState("");
@@ -8,82 +7,71 @@ function EnrollPage() {
   const [email, setEmail] = useState("");
   const [introduction, setIntroduction] = useState("");
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
   };
 
   return (
-    <div
-      className="form-container"
-      style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-    >
+    <div className="flex justify-center items-center ml-400 h-80vh">
       <Link to="/">
-        <div
-          style={{
-            position: "absolute",
-            top: "4%",
-            left: "10%",
-            transform: "translate(-50%, -50%)",
-            color: "black",
-            fontWeight: "bolder",
-            fontSize: "1rem",
-            cursor: "pointer",
-          }}
-        >
-          Remember <span style={{ color: "skyblue" }}>plus+</span>
+        <div className="absolute top-4 left-20 transform -translate-x-1/2 -translate-y-1/2 text-black font-bold text-lg cursor-pointer">
+          Remember <span className="text-blue">plus+</span>
         </div>
       </Link>
-      <div className="enroll-container">
+      <div className="bg-white p-8 w-800px rounded-15 text-center mt-80px shadow-md rounded-md">
         <h2 className="remember">
           Remember <span className="plus">plus+</span>
         </h2>
         <h2 className="enroll">인적사항</h2>
         <form onSubmit={handleSubmit}>
           <div className="enroll-form-group">
-            <img className="example-picture" src="https://via.placeholder.com/339x189" />
-            <div
-              className="enroll-form-group"
-              style={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-            >
+            <img
+              className="example-picture"
+              src="https://via.placeholder.com/339x189"
+              alt="Example"
+            />
+            <div className="enroll-form-group flex justify-center items-center mb-3">
               등록할 명함 사진
             </div>
             <input type="file" id="photo" name="photo" />
           </div>
-          <div className="enroll-form-group">
+
+          <div className="enroll-form-group text-left mb-3">
             <label className="label">이름*</label>
             <input
               type="name"
-              className="enroll-input"
+              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
               value={name}
               onChange={(event) => setName(event.target.value)}
             />
           </div>
-          <div className="enroll-form-group">
+          <div className="enroll-form-group text-left mb-3">
             <label className="label">개인 전화번호*</label>
             <input
               type="phone"
-              className="enroll-input"
+              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
               value={phone}
               onChange={(event) => setPhone(event.target.value)}
             />
           </div>
-          <div className="enroll-form-group">
+          <div className="enroll-form-group text-left mb-3">
             <label className="label">이메일</label>
             <input
               type="email"
-              className="enroll-input"
+              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
               value={email}
               onChange={(event) => setEmail(event.target.value)}
             />
           </div>
-          <div className="enroll-form-group">
+          <div className="enroll-form-group text-left mb-3">
             <label className="label">자기 소개</label>
             <textarea
-              className="enroll-input"
+              className="enroll-input w-100 h-32 border border-gray-300 shadow-md rounded-md"
               value={introduction}
               onChange={(event) => setIntroduction(event.target.value)}
             ></textarea>
           </div>
+
           <Link to="/second">
             <button type="submit" className="enrollButton">
               명함 등록
