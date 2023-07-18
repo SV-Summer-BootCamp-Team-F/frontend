f0ad424d97c51c4a36a0eaf1e14eef3434f3cf10
ajaj0123 <jenny3976255@gmail.com>
2023-07-19 00:23:30 +0900

    design: 가운데 정렬 및 코드 정렬 수정

--- src/pages/Card/EnrollPage.tsx
+++ src/pages/Card/EnrollPage.tsx
@@ -44,59 +44,61 @@ function EnrollPage() {
   };
 
   return (
-    <div className="flex justify-center items-center ml-400 h-80vh">
+    <div className="w-screen flex justify-center items-center">
       <Link to="/">
         <div className="absolute top-4 left-20 transform -translate-x-1/2 -translate-y-1/2 text-rememberBlack font-bold text-lg cursor-pointer">
           Remember <span className="text-rememberBlue">plus+</span>
         </div>
       </Link>
-      <div className="bg-white p-40 w-800px rounded-15 text-center mt-80px shadow-md rounded-md">
+      <div className="bg-white p-40 max-w-[800px] my-[20px] text-center mt-80px shadow-md rounded-md">
         <h2 className="remember">
           Remember <span className="plus">plus+</span>
         </h2>
         <h2 className="enroll">인적사항</h2>
         <form onSubmit={handleSubmit}>
           <div className="enroll-form-group">
-            <img id="preview-image" className="example-picture" src={previewImage} alt="Example" />
+            <img src={previewImage} alt="Example" />
             <input type="file" id="photo" name="photo" onChange={handleImageUpload} />
           </div>
-          <div className="enroll-form-group text-left mb-3">
-            <label className="label">이름*</label>
-            <input
-              type="name"
-              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
-              value={name}
-              onChange={handleNameChange}
-            />
-          </div>
-          <div className="enroll-form-group text-left mb-3">
-            <label className="label">개인 전화번호*</label>
-            <input
-              type="phone"
-              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
-              value={phone}
-              onChange={handlePhoneChange}
-            />
-          </div>
-          <div className="enroll-form-group text-left mb-3">
-            <label className="label">이메일</label>
-            <input
-              type="email"
-              className="enroll-input w-100 h-12 border border-gray-300 shadow-md rounded-md"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="enroll-form-group text-left mb-3">
-            <label className="label">자기 소개</label>
-            <textarea
-              className="enroll-input w-100 h-32 border border-gray-300 shadow-md rounded-md"
-              value={introduction}
-              onChange={handleIntroductionChange}
-            ></textarea>
+          <div className="enroll-item">
+            <div className="text-left mb-3">
+              <label className="label">이름*</label>
+              <input
+                type="name"
+                className="w-100 h-12 border border-gray-300 shadow-md rounded-md"
+                value={name}
+                onChange={handleNameChange}
+              />
+            </div>
+            <div className="text-left mb-3">
+              <label className="label">개인 전화번호*</label>
+              <input
+                type="phone"
+                className="w-100 h-12 border border-gray-300 shadow-md rounded-md"
+                value={phone}
+                onChange={handlePhoneChange}
+              />
+            </div>
+            <div className="text-left mb-3">
+              <label className="label">이메일</label>
+              <input
+                type="email"
+                className="w-100 h-12 border border-gray-300 shadow-md rounded-md"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="text-left mb-3">
+              <label className="label">자기 소개</label>
+              <textarea
+                className="w-100 h-32 border border-gray-300 shadow-md rounded-md"
+                value={introduction}
+                onChange={handleIntroductionChange}
+              ></textarea>
+            </div>
           </div>
           <Link to="/second">
-            <button type="submit" className="enrollButton text-rememberBlue border-gray-300">
+            <button type="submit" className="text-rememberBlue border-gray-300">
               명함 등록
             </button>
           </Link>
