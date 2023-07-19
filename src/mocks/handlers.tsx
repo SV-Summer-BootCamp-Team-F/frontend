9e4df964f7ebe61bac4f148d0e5579c0eefd0dea
rachel4w2 <rachel4w2@naver.com>
2023-07-20 01:18:29 +0900

    feat: msw 적용(테스트만 완료한 상태)

---
+++ src/mocks/handlers.tsx
@@ -0,0 +1,22 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+
+const handlers = [
+  rest.get("/api/products", (req, res, ctx) => {
+    const errorCode = req.url.searchParams.get("error_code");
+
+    if (errorCode) {
+      return res(ctx.status(Number(errorCode)));
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.delay(4000),
+      ctx.json({
+        items: [{ name: "product-1" }, { name: "product-2" }],
+      })
+    );
+  }),
+];
+
+export default handlers;
